28ee3ee6a787e11004ecf32e3fbbd62673869e37
lavjams <lavenderjams@gmail.com>
2015-06-17 09:09:26 -0400

    Added sum_digits.py and wordfreq.py to online github!

---
+++ sum_digits.py
@@ -0,0 +1,29 @@
+#Imports necessary functions
+import sys
+
+#Function: Adds digits of a given number
+def sum_digits(x):
+	string = str(x)
+	sumhere = 0
+	for a in range(0, len(string)):
+		sumhere = sumhere + int(string[a])
+		
+	return sumhere
+
+
+#Script for adding integer multiples of given numbers
+args = sys.argv
+arg1 = int(args[1])
+arg2 = None
+
+#For one-argument case
+if len(args) <= 2:
+	arg2 = arg1*arg1
+elif len(args) > 2:
+	arg2 = int(args[2])
+	
+#Below loops through multiples of first given number
+for b in range(1, (arg2/arg1)+1):
+	numhere = arg1*b
+	print sum_digits(numhere)
+	
---
+++ wordfreq.py
@@ -0,0 +1,56 @@
+#File: wordfreq.py
+#Purpose: Meant to frequency of words in a file with more than three letters, ordered by frequency
+
+#Below imports necessary functions
+import sys
+args = sys.argv
+
+#Below opens and reads the file
+words = {} #Dictionary to hold words and frequencies
+filename = open(args[1], 'r')
+rawfile = filename.read()
+
+#Below takes out punctuation in read-in string
+openfile = ''
+for g in range(0, len(rawfile)):
+	if not rawfile[g].isalpha() and not rawfile[g].isspace(): #Takes only letters
+		continue
+	openfile = openfile + rawfile[g]
+split = openfile.split() #Splits by white space
+
+#Adds each word and frequency in current line to dictionary
+for c in range(0, len(split)):
+	#Skips word if contains 3 letters or less
+	if len(split[c]) <= 3:
+		continue
+		
+	#Adds word if not yet in dictionary
+	if split[c] not in words:
+		words[split[c]] = 1
+	#Increments frequency if word already in dictionary
+	elif split[c] in words:
+		words[split[c]] = words[split[c]] + 1
+				
+#Below sorts dictionary entries
+for d in range(0, len(words)):
+	currfreq = -1
+	currword = None
+	
+	#Below considers each key in dictionary
+	for key in words:
+		if currfreq <= words[key]: #More frequent word found
+			currword = key
+			currfreq = words[key]
+	
+	#Below 'deletes' current chosen word from consideration
+	words[currword] = (-1)*float('inf')
+	
+	#Below prints most frequent word found
+	print(currword + ': ' + str(currfreq))
+	if d > 20:
+		break
+	
+			
+	
+
+
